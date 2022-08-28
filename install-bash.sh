9008cd8f1bcc1ec92fe80873d6af00097033b9a2
t <87922964+PhotonMastr@users.noreply.github.com>
2022-08-28 19:15:32 -0400

    Delete git folder when the install is done

--- install-bash.sh
+++ install-bash.sh
@@ -4,5 +4,6 @@ pyinstaller --onefile ConvertU.py
 mv $HOME/ConvertU-CLI/dist/ConvertU $HOME/ConvertU_CLI/
 echo "alias ConvertU='~/ConvertU_CLI/./ConvertU'" >> $HOME/.bashrc
 source $HOME/.bashrc 
+rm -rf ~/ConvertU-CLI/
 echo "ConvertU-CLI is installed!"
 
