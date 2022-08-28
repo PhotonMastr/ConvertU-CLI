89763a662bd8c0373f01966f3c8cbdbd53d2a1d9
t <87922964+PhotonMastr@users.noreply.github.com>
2022-08-28 19:14:40 -0400

    Delete git folder when the install is done

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,4 +4,5 @@ pyinstaller --onefile ConvertU.py
 mv $HOME/ConvertU-CLI/dist/ConvertU $HOME/ConvertU_CLI/
 echo "alias ConvertU='~/ConvertU_CLI/./ConvertU'" >> $HOME/.zshrc
 source $HOME/.zshrc 
+rm -rf ~/ConvertU-CLI/
 echo "ConvertU-CLI is installed!"
