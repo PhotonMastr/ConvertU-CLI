d0b0d5148e816a557515d5b238349c023ad3cac3
t <87922964+PhotonMastr@users.noreply.github.com>
2022-08-28 19:15:52 -0400

    Delete git folder when the install is done

--- install-fish.sh
+++ install-fish.sh
@@ -4,4 +4,5 @@ pyinstaller --onefile ConvertU.py
 mv $HOME/ConvertU-CLI/dist/ConvertU $HOME/ConvertU_CLI/
 echo "alias ConvertU='~/ConvertU_CLI/./ConvertU'" >> $HOME/.config/fish/config.fish
 source $HOME/.config/fish/config.fish
+rm -rf ~/ConvertU-CLI/
 echo "ConvertU-CLI is installed!"
